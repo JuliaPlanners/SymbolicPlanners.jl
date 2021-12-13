1a8a78e49aaca9580ab80832302c889db8801c94
Xuan <tanqazx@gmail.com>
2021-12-12 21:30:02 -0500

    Update dependencies and compat section.

--- src/SymbolicPlanners.jl
+++ src/SymbolicPlanners.jl
@@ -2,7 +2,6 @@ module SymbolicPlanners
 
 using Base: @kwdef
 using Parameters: @unpack
-using Setfield: @set
 using DataStructures: PriorityQueue, OrderedDict, enqueue!, dequeue!
 using StatsBase: sample, Weights
 using Random, Julog, PDDL
