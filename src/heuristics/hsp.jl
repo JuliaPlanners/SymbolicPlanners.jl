a977a7b242abc0f18716373e81687822c9d30533
Xuan <tanqazx@gmail.com>
2021-12-17 16:58:14 -0500

    Extend HSP to domains with functional conditions.

--- src/heuristics/hsp.jl
+++ src/heuristics/hsp.jl
@@ -1,6 +1,7 @@
 ## HSP family of heuristics ##
 export HSPHeuristic, HAdd, HMax
 export HSPRHeuristic, HAddR, HMaxR
+export FunctionalHSP, FunctionalHAdd, FunctionalHMax
 
 "HSP family of relaxation heuristics."
 mutable struct HSPHeuristic <: Heuristic
@@ -102,3 +103,64 @@ HMaxR(args...) = HSPRHeuristic(maximum, args...)
 
 "HSPr heuristic where a fact's cost is the summed cost of its dependencies."
 HAddR(args...) = HSPRHeuristic(sum, args...)
+
+"Generalized HSP heuristic which handles functional conditions."
+mutable struct FunctionalHSP <: Heuristic
+    op::Function # Aggregator (e.g. maximum, sum) for fact costs
+    abstractions::Dict{Symbol,Type} # Abstractions to use
+    max_repeats::Int # Maximum number of action repeats
+    graph::PlanningGraph # Precomputed planning graph
+    goal_idxs::Set{Int} # Precomputed list of goal indices
+    pre_key::Tuple{UInt64,UInt64} # Precomputation hash
+    FunctionalHSP(op, abstractions, max_repeats) =
+        new(op, abstractions, max_repeats)
+end
+
+FunctionalHSP(op; abstractions=PDDL.DEFAULT_ABSTRACTIONS, max_repeats=10) =
+    FunctionalHSP(op, abstractions, max_repeats)
+
+Base.hash(heuristic::FunctionalHSP, h::UInt) =
+    hash(heuristic.op, hash(heuristic.abstractions, hash(FunctionalHSP, h)))
+
+function precompute!(h::FunctionalHSP,
+                     domain::Domain, state::State, spec::Specification)
+    # Check if cache has already been computed
+    if is_precomputed(h, domain, state, spec) return h end
+    # Precomputed data is unique to each domain and specification
+    h.pre_key = (objectid(domain), objectid(spec))
+    # Build planning graph and find goal condition indices
+    goal_conds = PDDL.to_cnf_clauses(get_goal_terms(spec))
+    h.graph = build_planning_graph(domain, state, goal_conds)
+    h.goal_idxs = Set(findall(c -> c in goal_conds, h.graph.conditions))
+    return h
+end
+
+function is_precomputed(h::FunctionalHSP,
+                        domain::Domain, state::State, spec::Specification)
+    return (isdefined(h, :graph) &&
+            objectid(domain) == h.pre_key[1] &&
+            objectid(spec) == h.pre_key[2])
+end
+
+function compute(h::FunctionalHSP,
+                 domain::Domain, state::State, spec::Specification)
+    # Precompute if necessary
+    if !is_precomputed(h, domain, state, spec)
+        precompute!(h, domain, state, spec)
+    end
+    # Compute relaxed costs to each condition node of the planning graph
+    costs, _ = relaxed_functional_graph_search(
+        domain, state, spec,
+        h.op, h.graph, h.abstractions,
+        h.max_repeats, h.goal_idxs
+    )
+    # Return goal cost (may be infinite if unreachable)
+    goal_cost = h.op(costs[g] for g in h.goal_idxs)
+    return goal_cost
+end
+
+"Generalized [`HMax`](@ref) heuristic that supports functional conditions."
+FunctionalHMax(; options...) = FunctionalHSP(maximum; options...)
+
+"Generalized [`HAdd`](@ref) heuristic that supports functional conditions."
+FunctionalHAdd(; options...) = FunctionalHSP(sum; options...)
--- src/heuristics/utils.jl
+++ src/heuristics/utils.jl
@@ -7,6 +7,7 @@ struct PlanningGraph
     act_children::Vector{Vector{Int}} # Child conditions of each action
     conditions::Vector{Term} # All ground preconditions / goal conditions
     cond_children::Vector{Vector{Int}} # Child actions of each condition
+    is_func::BitVector # Whether condition is a function
 end
 
 """
@@ -53,6 +54,7 @@ function build_planning_graph(domain::Domain, state::State,
     # Flatten map from conditions to child indices
     cond_children = [sort!(collect(is)) for is in values(cond_map)]
     conditions = collect(keys(cond_map))
+    is_func = falses(length(conditions))
     # Determine parent and child conditions of each action
     act_parents = [Int[] for _ in 1:length(actions)]
     act_children = [Int[] for _ in 1:length(actions)]
@@ -68,6 +70,7 @@ function build_planning_graph(domain::Domain, state::State,
         elseif cond.name == true || cond.name == false # Handle constants
             idxs = get(effect_map, cond, Set{Int}())
         else # Handle functional terms
+            is_func[i] = true
             terms = PDDL.constituents(cond, domain)
             idxs = reduce(union, (get(effect_map, t, Set{Int}()) for t in terms))
         end
@@ -76,7 +79,7 @@ function build_planning_graph(domain::Domain, state::State,
     act_children = unique!.(sort!.(act_children))
     # Construct and return graph
     return PlanningGraph(actions, act_parents, act_children,
-                         conditions, cond_children)
+                         conditions, cond_children, is_func)
 end
 
 "Compute relaxed costs and paths to each fact node of a planning graph."
@@ -133,6 +136,128 @@ function relaxed_graph_search(
                     queue[c_idx] = next_cost
                 end
                 delete!(unreached, c_idx) # Removed any reached goals
+            end
+        end
+    end
+
+    # Return fact costs and achievers
+    return costs, achievers
+end
+
+"Relaxed graph search with functional conditions."
+function relaxed_functional_graph_search(
+    domain::Domain, state::State, spec::Specification,
+    accum_op::Function, graph::PlanningGraph,
+    abstractions::Dict, max_repeats::Int, goal_idxs=nothing
+)
+    # Initialize fact costs, counters,  etc.
+    costs = fill(Inf, length(graph.conditions)) # Fact costs
+    dists = accum_op === maximum ? costs : fill(-1, length(graph.conditions))
+    achievers = fill(-1, length(graph.conditions)) # Fact achievers
+    counters = [length(a.preconds) for a in graph.actions] # Action counters
+    repeats = fill(0, length(graph.actions)) # Action repetitinos
+
+    # Set up initial facts and priority queue
+    init_idxs = _get_init_idxs(graph, domain, state)
+    costs[init_idxs] .= 0
+    if (accum_op !== maximum) dists[init_idxs] .= 0 end
+    queue = PriorityQueue(i => dists[i] for i in init_idxs)
+
+    # Check if any goal conditions are already reached
+    if goal_idxs !== nothing
+        for g in goal_idxs
+            goal_cond = graph.conditions[g]
+            costs[g] == Inf && !satisfy(domain, state, goal_cond) && continue
+            costs[g] = 0
+            if (accum_op !== maximum) dists[g] = 0 end
+        end
+        unreached = copy(goal_idxs)
+    else
+        unreached = Set{Int}(-1) # Set with dummy unreachable node
+    end
+
+    # Construct abstracted domain and state
+    domain = domain isa GenericDomain ? domain : PDDL.get_source(domain)
+    state = state isa GenericState ? state : GenericState(state)
+    absdom = abstracted(domain; abstractions=abstractions)
+    abstate = abstractstate(absdom, state)
+
+    # Define action expansion subroutine
+    function expand_act!(act_idx)
+        # Increment repetition counter
+        repeats[act_idx] += 1
+        is_max_rep = repeats[act_idx] >= max_repeats
+
+        # Compute path cost of achieved action
+        act_parents = graph.act_parents[act_idx]
+        path_cost = accum_op(costs[p] for p in act_parents)
+        act_cost = has_action_cost(spec) ?
+            get_action_cost(spec, domain, graph.actions[act_idx].term) : 1
+        next_cost = path_cost + act_cost * repeats[act_idx]
+
+        # Compute path distance
+        path_dist = accum_op === maximum ?
+            path_cost : maximum(dists[p] for p in act_parents)
+        next_dist = accum_op === maximum ?
+            next_cost : path_dist + repeats[act_idx]
+
+        # Widen abstract state with functional effects of action
+        diff = graph.actions[act_idx].effect
+        vals = [evaluate(absdom, abstate, v) for v in values(diff.ops)]
+        for (term, val) in zip(keys(diff.ops), vals)
+            widened = PDDL.widen(abstate[term], val)
+            abstate[term] = widened
+        end
+
+        # Place child conditions on queue
+        act_children = graph.act_children[act_idx]
+        requeue_act = false
+        for c_idx in act_children
+            # Skip conditions which are already reached more cheaply
+            if next_cost > costs[c_idx] continue end
+            # Handle functional conditions
+            if (!is_max_rep && graph.is_func[c_idx] &&
+                !satisfy(absdom, abstate, graph.conditions[c_idx]))
+                # Requeue action if functional condition not satisfied
+                requeue_act = true
+                continue
+                # Otherwise assume condition satisfied (incl. after max reps)
+            end
+            # Store new cost, distance and achiever
+            costs[c_idx] = next_cost
+            if (accum_op !== maximum) dists[c_idx] = next_dist end
+            achievers[c_idx] = act_idx # Store new cheapest achiever
+            if !(c_idx in keys(queue)) # Enqueue new conditions
+                enqueue!(queue, c_idx, next_dist)
+            else # Reduce the cost of those in queue
+                queue[c_idx] = next_dist
+            end
+            delete!(unreached, c_idx) # Removed any reached goals
+        end
+
+        # Requeue action if necessary
+        if requeue_act
+            if !(-act_idx in keys(queue))
+                enqueue!(queue, -act_idx, next_dist)
+            else
+                queue[-act_idx] = next_dist
+            end
+        end
+    end
+
+    # Perform Djikstra / uniform-cost search until goals are reached
+    while !isempty(queue) && !isempty(unreached)
+        # Dequeue lowest cost fact/condition
+        cond_idx = dequeue!(queue)
+        if cond_idx < 0 # Convention: negative indices are repeated actions
+            act_idx = -cond_idx
+            expand_act!(act_idx)
+        else # Iterate over child actions
+            for act_idx in graph.cond_children[cond_idx]
+                # Decrease counter for unachieved actions
+                counters[act_idx] -= 1
+                counters[act_idx] != 0 && continue
+                expand_act!(act_idx)
             end
         end
     end
