5909ef9af7aacb0db024c5248cfc2d23bda9a052
Xuan <tanqazx@gmail.com>
2022-12-19 01:05:36 -0500

    Add `get_action_probs` to PolicySolution interface.

--- src/solutions/random_policy.jl
+++ src/solutions/random_policy.jl
@@ -10,5 +10,18 @@ RandomPolicy(domain) = RandomPolicy(domain, Random.GLOBAL_RNG)
 
 get_action(sol::RandomPolicy, state::State) =
     rand_action(sol, state)
-rand_action(sol::RandomPolicy, state::State) =
-    rand(sol.rng, collect(available(sol.domain, state)))
+
+function rand_action(sol::RandomPolicy, state::State)
+    actions = available(sol.domain, state)
+    if Base.IteratorSize(actions) == Base.SizeUnknown()
+        actions = collect(actions)
+    end
+    return rand(sol.rng, actions)
+end
+
+function get_action_probs(sol::RandomPolicy, state::State)
+    probs = Dict(act => 1.0 for act in available(sol.domain, state))
+    n_actions = length(probs)
+    map!(x -> x / n_actions, values(probs))
+    return probs
+end
