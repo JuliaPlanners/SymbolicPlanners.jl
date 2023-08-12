7ca9630bd91af981aa158348f273608554c4022a
Xuan <tanqazx@gmail.com>
2023-08-12 10:45:46 -0400

    Manually update versions.js.

--- versions.js
+++ versions.js
@@ -1,8 +1,8 @@
 var DOC_VERSIONS = [
   "stable",
-  "v1.14",
+  "v1.15",
   "v0.1",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v01.14";
+var DOCUMENTER_NEWEST = "v01.15";
 var DOCUMENTER_STABLE = "stable";
