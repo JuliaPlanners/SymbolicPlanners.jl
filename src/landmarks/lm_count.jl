935a17469b5aa42f043f5ecb0a3008b3eb056355
Bart van Maris <Bartvanmaris@hotmail.nl>
2024-01-09 20:06:45 +0100

    miniscule timesave

--- src/landmarks/lm_count.jl
+++ src/landmarks/lm_count.jl
@@ -5,10 +5,11 @@ export LMCount
     lm_status_manager::LandmarkStatusManager
     prev_state
     is_precomputed::Bool
+    nr_nodes::Int
 end
 
 function LMCount(lm_graph::LandmarkGraph, p_graph::PlanningGraph)
-    return LMCount(lm_graph, LandmarkStatusManager(lm_graph, p_graph), nothing, false)
+    return LMCount(lm_graph, LandmarkStatusManager(lm_graph, p_graph), nothing, false, 0)
 end
 
 function compute(h::LMCount,
@@ -18,33 +19,30 @@ function compute(h::LMCount,
         println("Previous state not updated in planner!")
         return 0
     else 
+        #Previous state of this Heuristic should be updated in the planner that uses it, See "Forward.jl" at line 243
         progress(h.lm_status_manager, h.prev_state, state)
         future = get_future_landmarks(h.lm_status_manager, state)
         past = get_past_landmarks(h.lm_status_manager, state)
 
-        # n = Number of all landmarks found
-        n = length(h.lm_graph.nodes)
-
-        # m = Number of nodes that are in lm_status_manager.past 
-        m = length(past)
-    
-        # k = Number of nodes that are in lm_status_manager.past and also in lm_status_manager.future
-        future_past = intersect(future, past)
-        k = length(future_past)
-        #Previous state of this Heuristic should be updated in the planner that uses it, See "Forward.jl" at line 243
-        #return H value
         
-        # println("Step: ------------")
-        # print_past::Set{Int} = Set()
-        # print_future::Set{Int} = Set()
-        # for node::Tuple{Int, LandmarkNode} in enumerate(h.lm_graph.nodes)
-        #     if node[2] in past push!(print_past, node[1]) end
-        #     if node[2] in future push!(print_future, node[1]) end
-        # end
-        # println("Past: $(print_past)")
-        # println("Future: $(print_future)")
-        # println("Eq: N->$n, M->$m, K->$k, Res:$(n-m+k)")
-        return n - m + k
+        h_val = 0
+        for i in 1:h.nr_nodes
+            if i ∉ past h_val += 1
+            elseif i in future h_val += 1 end
+        end
+        # # n = Number of all landmarks found
+        # n = length(h.lm_graph.nodes)
+        
+        # # m = Number of nodes that are in lm_status_manager.past 
+        # m = length(past)
+            
+        # # k = Number of nodes that are in lm_status_manager.past and also in lm_status_manager.future
+        # future_past = intersect(future, past)
+        # k = length(future_past)
+
+        #return H value
+        #return n - m + k
+        return h_val
     end
 end
 
@@ -61,6 +59,7 @@ function precompute!(h::LMCount, domain::Domain, state::State)
     end
     progress_initial_state(h.lm_status_manager, state)
     h.prev_state = state
+    h.nr_nodes = length(h.lm_graph.nodes)
     h.is_precomputed = true
     return h
 end
